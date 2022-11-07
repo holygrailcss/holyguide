fa7da4736ff6775094bf507cade7798db2007a22
manuelruizredondo <manuelruizredondo@gmail.com>
2022-11-07 18:15:07 +0100

    cambios en flexbox

---
+++ src/assets/js/flexbox-actions.js
@@ -0,0 +1,101 @@
+function isFlex() {
+	$(".flexbox-demo").toggleClass("is-flex");
+	$(".flexbox-demo-is-flex").toggleClass("hidden ");
+  $(this).addClass("activeflex ");
+}
+function isFlexInline() {
+	$(".flexbox-demo").toggleClass("is-flexinline");
+	$(".flexbox-demo-is-flexinline").toggleClass("hidden ");
+  $(this).toggleClass("activeflex ");
+}
+function isFlexRow() {
+	$(".flexbox-demo").toggleClass("flex-row");
+	$(".flexbox-demo-is-flex-row").toggleClass("hidden ");
+  $(this).toggleClass("activeflex ");
+}
+function isFlexColumn() {
+	$(".flexbox-demo").toggleClass("flex-column");
+	$(".flexbox-demo-is-flex-column").toggleClass("hidden ");
+  $(this).toggleClass("activeflex ");
+}
+function isFlexWrap() {
+	$(".flexbox-demo").toggleClass("flex-wrap");
+	$(".flexbox-demo-is-wrap").toggleClass("hidden ");
+  $(this).toggleClass("activeflex ");
+
+}
+
+function isFlexNoWrap() {
+	$(".flexbox-demo").toggleClass("flex-nowrap");
+	$(".flexbox-demo-is-nowrap").toggleClass("hidden ");
+  $(this).toggleClass("activeflex ");
+}
+function isFlexContentStart() {
+	$(".flexbox-demo").toggleClass("content-start");
+	$(".flexbox-demo-is-content-start").toggleClass("hidden ");
+}
+function isFlexContentEnd() {
+	$(".flexbox-demo").toggleClass("content-end");
+	$(".flexbox-demo-is-content-end").toggleClass("hidden ");
+}
+function isFlexContentCenter() {
+	$(".flexbox-demo").toggleClass("content-center");
+	$(".flexbox-demo-is-content-center").toggleClass("hidden ");
+}
+function isFlexContentBetween() {
+	$(".flexbox-demo").toggleClass("content-sp-between");
+	$(".flexbox-demo-is-content-between").toggleClass("hidden ");
+}
+function isFlexContentAround() {
+	$(".flexbox-demo").toggleClass("content-sp-around");
+	$(".flexbox-demo-is-content-around").toggleClass("hidden ");
+}
+function isFlexContentEvenly() {
+	$(".flexbox-demo").toggleClass("content-sp-evenly");
+	$(".flexbox-demo-is-content-evenly").toggleClass("hidden ");
+}
+function isFlexAlignTop() {
+	$(".flexbox-demo").toggleClass("align-top");
+	$(".flexbox-demo-is-align-top").toggleClass("hidden ");
+}
+function isFlexAlignMiddle() {
+	$(".flexbox-demo").toggleClass("align-middle");
+	$(".flexbox-demo-is-align-middle").toggleClass("hidden ");
+}
+function isFlexAlignMultiBottom() {
+	$(".flexbox-demo").toggleClass("align-multi-bottom");
+	$(".flexbox-demo-is-align-bottom").toggleClass("hidden ");
+}
+
+function isFlexAlignCenter() {
+	$(".flexbox-demo").toggleClass("align-center");
+	$(".flexbox-demo-is-align-center").toggleClass("hidden ");
+}
+
+
+
+
+function isFlexAlignMultiTop() {
+	$(".flexbox-demo").toggleClass("align-multi-top");
+	$(".flexbox-demo-is-align-multi-top").toggleClass("hidden ");
+}
+function isFlexAlignBottom() {
+	$(".flexbox-demo").toggleClass("align-bottom");
+	$(".flexbox-demo-is-align-bottom").toggleClass("hidden ");
+}
+function isFlexAlignMultiCenter() {
+	$(".flexbox-demo").toggleClass("align-multi-center");
+	$(".flexbox-demo-is-align-multi-center").toggleClass("hidden ");
+}
+function isFlexAlignMultiBetween() {
+	$(".flexbox-demo").toggleClass("align-multi-between");
+	$(".flexbox-demo-is-align-multi-between").toggleClass("hidden ");
+}
+function isFlexAlignMultiAround() {
+	$(".flexbox-demo").toggleClass("align-multi-around");
+	$(".flexbox-demo-is-align-multi-around").toggleClass("hidden ");
+}
+function isFlexAlignMultiStretch() {
+	$(".flexbox-demo").toggleClass("align-multi-stretch");
+	$(".flexbox-demo-is-align-multi-stretch").toggleClass("hidden ");
+}
