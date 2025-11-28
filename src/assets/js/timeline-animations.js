a4512d09620d49e64ad417878e91c587cbaf0feb
pabloblop <pabloblop@ext.massimodutti.com>
2025-11-28 07:45:15 +0100

    Ajuste timeline

--- src/assets/js/timeline-animations.js
+++ src/assets/js/timeline-animations.js
@@ -1,12 +1,12 @@
 gsap.registerPlugin(ScrollTrigger);
 
 const initTimelineAnimations = () => {
-  const items = gsap.utils.toArray('.md-timeline2-item');
+  const items = gsap.utils.toArray('.md-timeline2-point');
   
   if (!items.length) return;
 
   items.forEach((item) => {
-    const figure = item.querySelector('.md-timeline2-point');
+    const figure = item.querySelector('.md-timeline2-point-media-figure');
     
     if (!figure) {
       return;
@@ -22,52 +22,65 @@ const initTimelineAnimations = () => {
         end: "center top-=70%",
         scrub: true,
         invalidateOnRefresh: true,
+        onEnter: (self) => {
+          const distance = self.end - self.start;
+          console.log('Distancia total de scroll:', distance, 'px');
+        }
       }
     });
     
     tl.fromTo(figure, {
       scale: 0.5,
-      y: -offset * 0.72,
+      y: -offset * 0.9,
+      borderLeft: "solid 1px black",
       ease: "none",
     },
             {
               scale: 0.6666,
-              y: -offset * 0.35,
+              y: -offset * 0.40,
               ease: "none",
+              borderLeft: "solid 10px red",
             })
                     .to(figure,{
                       scale: 0.8,
-                      y: -offset * 0.14,
+                      y: -offset * 0.15,
                       ease: "none",
+                      borderLeft: "solid 1px blue",
                     })
                             .to(figure,{
                               scale: 0.9,
-                              y: -offset * 0.07,
+                              y: -offset * 0.06,
                               ease: "none",
+                              borderLeft: "solid 10px green",
                             })
                                     .to(figure, {
                                       scale: 1,
                                       y: 0, // 0 ----------------
                                       ease: "none",
+                                      borderLeft: "solid 1px orange",
                                     })
                             .to(figure, {
                               scale: 0.9,
-                              y: offset * 0.07,
+                              y: offset * 0.06,
                               ease: "none",
+                              borderLeft: "solid 10px green",
                             })
                     .to(figure, {
                       scale: 0.8,
-                      y: offset * 0.13,
+                      y: offset * 0.15,
                       ease: "none",
+                      borderLeft: "solid 1px blue",
                     })
             .to(figure, {
               scale: 0.6666,
-              y: offset * 0.35,
+              y: offset * 0.40,
               ease: "none",
+              borderLeft: "solid 10px red",
             })
     .to(figure, {
       scale: 0.5,
-      y: offset * 0.72,
+      y: offset * 0.9,
+      borderLeft: "solid 1px black",
       ease: "none",
     });
   });
